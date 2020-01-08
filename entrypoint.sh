377841ba72e4f1a44a86288aeafaea6957374157
leslie tsang <noreply@ngx.ai>
2020-01-08 20:20:01 +0800

    [update] ansible container add ssh-agent support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# init ssh-agent service
 eval `ssh-agent -s`
+
+# try to load default private key
+if [[ -e ${ANSIBLE_REMOTE_PRIVATE_KEY} ]]; then
+	ssh-add ${ANSIBLE_REMOTE_PRIVATE_KEY}
+fi
 
 exec "$@"
